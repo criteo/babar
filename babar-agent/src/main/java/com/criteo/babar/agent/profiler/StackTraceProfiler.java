48ff57ad65afa316311ff1ef2007185f16a72d78
Benoit Hanotte <b.hanotte@criteo.com>
2018-04-06 10:56:28 +0200

    Refactored StackTraceProfiler

--- babar-agent/src/main/java/com/criteo/babar/agent/profiler/StackTraceProfiler.java
+++ babar-agent/src/main/java/com/criteo/babar/agent/profiler/StackTraceProfiler.java
@@ -3,21 +3,22 @@ package com.criteo.babar.agent.profiler;
 import com.criteo.babar.agent.config.AgentConfig;
 import com.criteo.babar.agent.profiler.utils.ThreadUtils;
 import com.criteo.babar.agent.reporter.Reporter;
+import com.criteo.babar.agent.worker.SamplingScheduler;
+import com.google.common.base.Joiner;
 
 import java.lang.management.ThreadInfo;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StackTraceProfiler extends SamplingAggregatingProfiler {
 
-    private static final List<String> EXCLUDE_PACKAGES = Arrays.asList("com.bhnte", "io.grpc");
-
-    private final StackTraceFilter filter;
-    private TraceCache traceCache;
+    private final TraceCache traceCache;
 
     public StackTraceProfiler(AgentConfig agentConfig, Reporter reporter) {
         super(agentConfig, reporter);
-
-        filter = new StackTraceFilter(new ArrayList<String>(), EXCLUDE_PACKAGES);
         traceCache = new TraceCache();
     }
 
@@ -26,24 +27,25 @@ public class StackTraceProfiler extends SamplingAggregatingProfiler {
         for (ThreadInfo thread : getAllRunnableThreads()) {
             // certain threads do not have stack traceCache
             if (thread.getStackTrace().length > 0) {
-                String traceKey = StackTraceFormatter.formatStackTrace(thread.getThreadName(), thread.getStackTrace());
-                if (filter.includeStackTrace(traceKey)) {
-                    traceCache.increment(traceKey);
-                }
+                String traceKey = formatStackTrace(thread.getThreadName(), thread.getStackTrace());
+                traceCache.increment(traceKey);
             }
         }
     }
 
     @Override
     public void report() throws Exception {
-        Map<String, Long> traces = traceCache.clear();
+        Map<String, Long> traces = traceCache.copyAndclear();
         for (Map.Entry<String, Long> trace: traces.entrySet()) {
             reporter.reportEvent("CPU_TRACES", trace.getKey(), trace.getValue().doubleValue(), System.currentTimeMillis());
         }
     }
 
-    private static Collection<ThreadInfo> getAllRunnableThreads() {
-        return ThreadUtils.filterAllThreadsInState(false, false, Thread.State.RUNNABLE, ThreadUtils.EXCLUDED_THREADS_PREFIXES);
+    private Collection<ThreadInfo> getAllRunnableThreads() {
+        return ThreadUtils.getAllThreads(false, false,
+                t -> t.getThreadState() == Thread.State.RUNNABLE &&
+                        !t.getThreadName().startsWith(SamplingScheduler.SCHEDULER_THREAD_PREFIX)
+        );
     }
 
     @Override
@@ -55,16 +57,33 @@ public class StackTraceProfiler extends SamplingAggregatingProfiler {
         report();
     }
 
+    private static String formatStackTrace(String threadName, StackTraceElement[] stack) {
+        Deque<String> lines = new ArrayDeque<>(stack.length);
+        for (StackTraceElement element : stack) {
+            lines.addFirst(formatStackTraceElement(element));
+        }
+        lines.addFirst(threadName);
+
+        return Joiner.on("|").join(lines);
+    }
+
+    private static String formatStackTraceElement(StackTraceElement element) {
+        return String.format("%s.%s:%d", element.getClassName(), element.getMethodName(), element.getLineNumber());
+    }
+
     private class TraceCache {
+        // instead of using ConcurrentHashMap, we synchronize on the map object before
+        // each operation since ConcurrentHashMap does not provide an atomic equivalent to
+        // copyAndclear()
         private Map<String, Long> traces = new HashMap<>();
 
         void increment(String trace) {
             synchronized (traces) {
-                traces.put(trace, traces.getOrDefault(trace, 0l) + 1l);
+                traces.compute(trace, (k, v) -> ((v != null) ? v : 0L) + 1L);
             }
         }
 
-        Map<String, Long> clear() {
+        Map<String, Long> copyAndclear() {
             Map<String, Long> tmp;
             synchronized (traces) {
                 tmp = traces;
