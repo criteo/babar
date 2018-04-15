f87fad972888c0cf60bcdd504a24d07a1f7803ae
Benoit Hanotte <benoit.hnte@gmail.com>
2018-04-15 16:39:37 +0200

    Changed random host name generation method

--- babar-agent/src/main/java/com/criteo/babar/agent/reporter/Reporter.java
+++ babar-agent/src/main/java/com/criteo/babar/agent/reporter/Reporter.java
@@ -11,21 +11,34 @@ public abstract class Reporter {
     public abstract void stop();
     public abstract void reportEvent(String metric, String label, Double value, Long time);
 
-    protected final String container;
+    protected static final String container;
+
+    static {
+        container = getContainerId();
+    }
 
     public Reporter(AgentConfig config) {
-        this.container = getContainerId();
         System.out.println("Using container id '" + container + "' in profiles");
     }
 
-    protected String getContainerId() {
+    private static String getContainerId() {
         try {
             String hostname = InetAddress.getLocalHost().getHostName();
-            return hostname + "_" + String.valueOf(Math.round(Math.random() * 10000));
+            return hostname + "_" + getRandomString(8);
         } catch (UnknownHostException e) {
             System.err.println("Unable to get the hostname, using random value instead");
             e.printStackTrace();
-            return String.valueOf(Math.round(Math.random() * 1000000000));
+            return getRandomString(32);
         }
+    }
+
+    private static String getRandomString(int length) {
+        char[] randomChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int j = (int)Math.round(Math.random() * randomChars.length);
+            sb.append(randomChars[j]);
+        }
+        return sb.toString();
     }
 }
