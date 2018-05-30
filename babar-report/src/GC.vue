e0d901c2c78e7502161c0b0ea9752c371a35b312
Benoit Hanotte <b.hanotte@criteo.com>
2018-05-31 00:21:28 +0200

    Added GC parameters to graph explanation

--- babar-report/src/GC.vue
+++ babar-report/src/GC.vue
@@ -11,10 +11,13 @@
       <b-container class="text-center" fluid>
         <PlotTimeSeries title="Minor & Major median GC ratio" yAxis="%time" yMax="1" :series="series.minorMajor" />
         <div class="explanation">
-          This graph shows the median ratio of wall-clock time spent doing minor and major garbage collections in the JVMs on all containers.<br>
-          <strong>Minor GC</strong> only clean the young generation, while <strong>Major GC</strong> cleans both the young and old ones.<br>
-          Major GC should be much less frequent that minor GC, otherwise it could indicate that too many short lived-objects are promototed to the old generation.
-          If this is the case, you may want to resize the generations and make sure that no humongous object uses most of the old generation.
+          This graph shows the median ratio of wall-clock time spent doing minor and major garbage collections in the JVMs over all containers.<br>
+          <strong>Minor GC</strong> only clean the young generation, while <strong>Major GC</strong> cleans both the young and old ones. The major GC 
+          should be much less frequent that the minor one, otherwise it could indicate that too many short lived-objects are promototed to the old generation.
+          If this is the case, you may want to resize the generations and make sure that no humongous object uses most of the old generation (which could 
+          trigger frequent major GC).<br>
+          You can tune the size of the generations either by specifying the <samp class="conf-value">-XX:NewRatio</samp> (integer value only) parameter or with the 
+          <samp class="conf-value">-XX:NewSize</samp> and <samp class="conf-value">-XX:MaxNewSize</samp> parameters for finer-grained tuning.
         </div>
         <PlotTimeSeries title="Accumulated JVM CPU time and GC CPU time" yAxis="sec" :series="series.accumulatedCpuGc" />
         <div class="explanation">
