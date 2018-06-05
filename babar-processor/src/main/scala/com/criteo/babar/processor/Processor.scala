54af446cc0ce5f4c58cf6450806365e48cf64887
Benoit Hanotte <b.hanotte@criteo.com>
2018-06-05 10:29:23 +0200

    Lower default minimum trace ratio to let more traces in report

--- babar-processor/src/main/scala/com/criteo/babar/processor/Processor.scala
+++ babar-processor/src/main/scala/com/criteo/babar/processor/Processor.scala
@@ -14,7 +14,7 @@ class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val outputFile = opt[String](short = 'o', descr = "path of the output file (default: ./babar_{date}.html)", default = Some(s"babar_${formatter.format(LocalDateTime.now())}.html"))
   val containers = opt[String](short = 'c', descr = "if set, only metrics of containers matching these prefixes are aggregated (comma-separated)", default = Some(""))
   val maxTracesDepth = opt[Int](short = 'd', descr = "max depth of stack traces", default = Some(100))
-  val minTracesRatio = opt[Double](short = 'r', descr = "min ratio of appearance in profiles for traces to be kept", default = Some(0.001))
+  val minTracesRatio = opt[Double](short = 'r', descr = "min ratio of appearance in profiles for traces to be kept", default = Some(0.00005))
   val logFile = trailArg[String](descr = "the log file to open")
   verify()
 }
